1b883018608a6c97850c56e9596794dd64a56ebb
Ryota Kameoka <kameoka.ryota@gmail.com>
2014-09-13 05:59:05 +0900

    Configure routing to papas#create_goal

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   post 'match_pin', to: 'papas#match_pin'
   get 'get_couple_by_husband', to: 'papas#get_couple_by_husband'
   get 'get_couple_by_wife', to: 'papas#get_couple_by_wife'
+  post 'create_goal', to: 'papas#create_goal'
 
 end
