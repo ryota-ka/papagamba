be2051d7d11feff412cf6f361fbf0faaf8ed3224
Ryota Kameoka <kameoka.ryota@gmail.com>
2014-09-13 03:43:03 +0900

    Implement feature of matching PIN

--- app/controllers/papas_controller.rb
+++ app/controllers/papas_controller.rb
@@ -5,4 +5,14 @@ class PapasController < ApplicationController
     render json: pin, only: [ :device_id, :pin ]
   end
 
+  def match_pin
+    pin = Pin.where(pin: params[:pin]).first()
+    if pin.present?
+      pin.destroy
+      render json: pin, only: [ :device_id, :pin ]
+    else
+      render json: { errors: [ :pin_not_found ] }
+    end
+  end
+
 end
